2b2154cd157a2599b79b3b176735c2412a9c9a6c
Torsten Feld <torsten@torsten-feld.de>
2015-06-18 17:08:22 +0200

    working on requirements for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,19 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'summics'
+gem 'minitest'
 require 'minitest/autorun'
 require 'webmock/minitest'
 require 'vcr'
-require 'turn'
-
-Turn.config do |c|
- # :outline  - turn's original case/test outline mode [default]
- c.format  = :outline
- # turn on invoke/execute tracing, enable full backtrace
- c.trace   = true
- # use humanized test names (works only with :outline format)
- c.natural = true
-end
+# require 'turn'
+#
+# Turn.config do |c|
+#  # :outline  - turn's original case/test outline mode [default]
+#  c.format  = :outline
+#  # turn on invoke/execute tracing, enable full backtrace
+#  c.trace   = true
+#  # use humanized test names (works only with :outline format)
+#  c.natural = true
+# end
 
 VCR.configure do |c|
   c.cassette_library_dir = "spec/fixtures"
