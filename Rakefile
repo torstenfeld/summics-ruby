c2211c0e2f382fc5d0ceffb00bc1feb8fba0f115
Torsten Feld <torsten@torsten-feld.de>
2015-06-18 21:50:12 +0200

    first tests working

--- Rakefile
+++ Rakefile
@@ -1,6 +1,16 @@
-require "bundler/gem_tasks"
-require "rspec/core/rake_task"
+# require 'bundler/gem_tasks'
+# require 'rspec/core/rake_task'
+#
+# RSpec::Core::RakeTask.new(:spec)
+#
+# task :default => :spec
 
-RSpec::Core::RakeTask.new(:spec)
 
-task :default => :spec
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.libs.push 'lib'
+  t.test_files = FileList['spec/*_spec.rb']
+  # t.test_files = FileList['test/*_test.rb']
+  t.verbose = true
+end
--- spec/summics_spec.rb
+++ spec/summics_spec.rb
@@ -1,17 +1,29 @@
 require_relative 'spec_helper'
+require_relative '../lib/summics'
 
 describe Summics do
   it 'has a version number' do
-    expect(Summics::VERSION).not_to be nil
+    Summics::VERSION.wont_be_nil
   end
-
-  # it 'does something useful' do
-  #   expect(false).to eq(true)
-  # end
 end
 
-class SummicsClientTest < Minitest::Test
-  def test_exists
-    assert Summics::Client
+describe 'get projects' do
+  let(:client) { Summics::Client.new('asdf', 'asdf') }
+
+  before do
+    VCR.insert_cassette 'projects', :record => :new_episodes
   end
+
+  after do
+    VCR.eject_cassette
+  end
+
+  it 'must have a projects method' do
+    client.must_respond_to :projects
+  end
+
+  it 'must parse the projects api response from JSON to Hash' do
+    client.projects.must_be_instance_of Array
+  end
+
 end
