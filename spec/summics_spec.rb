ad272406f6c85230e3e6021c98e03a6f80acb42e
Torsten Feld <torsten@torsten-feld.de>
2015-06-19 10:48:42 +0200

    added tests for addTexts method

--- spec/summics_spec.rb
+++ spec/summics_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'spec_helper'
+require 'ffaker'
 require_relative '../lib/summics'
 
 describe Summics do
@@ -72,7 +73,67 @@ describe 'Summics Methods' do
   # TODO: add test for texts with topics
   # TODO: add test for texts without topics
   # TODO: add test for dashboard
-  # TODO: add test for addTexts
+
+  describe 'add texts' do
+    let(:texts) {[{
+                      'text' => "As in indoor volleyball, the object of the game is to send the ball over the net and to ground it on the opponent's court, and to prevent the same effort by the opponent. A team is allowed up to three touches to return the ball across the net",
+                      'postId' => FFaker::Address.building_number,
+                      'timestamp' => DateTime.now.iso8601,
+                      'user' => FFaker::Address.building_number,
+                      'thisiscustom1' => FFaker::Internet.safe_email,
+                      '2ndcust' => FFaker::Product.product_name
+                  }, {
+                      'text' => "Volleyball Magazine staged the event the next year at the same location, this time sponsored by Schlitz Light Beer. In 1978 Wilk formed a sports promotion company named Event Concepts with Craig Masuoka and moved the World Championship of Beach Volleyball to Redondo Beach. Jose Cuervo tequila signed on as sponsor and the prize purse. The event was successful and Cuervo funded an expansion the next year to three events. The California Pro Beach Tour debuted with events in Laguna Beach, Santa Barbara and the World Championship in Redondo.",
+                      'postId' => FFaker::Address.building_number,
+                      'timestamp' => DateTime.now.iso8601,
+                      'user' => FFaker::Address.building_number,
+                      'thisiscustom1' => FFaker::Internet.safe_email,
+                      '2ndcust' => FFaker::Product.product_name
+                  }]}
+    let(:textsadded) { client.addTexts '5565b2dbca61f25d0302d1d9', texts }
+
+    before do
+      VCR.insert_cassette 'addtexts', :record => :new_episodes
+    end
+
+    after do
+      VCR.eject_cassette
+    end
+
+    it 'must have a addTexts method' do
+      client.must_respond_to :addTexts
+    end
+
+    it 'must parse the addTexts api response from JSON to Hash' do
+      textsadded.must_be_instance_of Hash
+    end
+
+    it 'must have properties imported and errors' do
+      textsadded.must_include 'imported'
+      textsadded.must_include 'errors'
+    end
+
+    describe 'errors' do
+      let(:errors) { textsadded['errors'] }
+
+      it 'must be an Array' do
+        errors.must_be_instance_of Array
+      end
+
+      it 'must be empty' do
+        errors.must_be_empty
+      end
+    end
+
+    describe 'imported' do
+      let(:taimported) { textsadded['imported'] }
+
+      it 'must have at least one result' do
+        taimported.wont_be_empty
+      end
+    end
+  end
+
   # TODO: add test for text with textid
   # TODO: add test for text with source and postid
 end
