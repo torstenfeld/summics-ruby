6b9c52043df60b587651daaa066c6b84163c36a2
Torsten Feld <torsten@torsten-feld.de>
2015-06-19 09:23:58 +0200

    extended tests for projects and started with tests for topics

--- spec/summics_spec.rb
+++ spec/summics_spec.rb
@@ -11,7 +11,6 @@ describe 'Summics Methods' do
   let(:client) { Summics::Client.new('asdf', 'asdf') }
 
   describe 'get projects' do
-    # let(:client) { Summics::Client.new('asdf', 'asdf') }
 
     before do
       VCR.insert_cassette 'projects', :record => :new_episodes
@@ -25,21 +24,46 @@ describe 'Summics Methods' do
       client.must_respond_to :projects
     end
 
-    it 'must parse the projects api response from JSON to Hash' do
+    it 'must parse the projects api response from JSON to Array' do
       client.projects.must_be_instance_of Array
+    end
+
+    it 'must have at least one result' do
+      client.projects.wont_be_empty
+    end
+
+    it 'must have one item with specific properties' do
+      first_item = client.projects[0]
+      first_item.must_be_instance_of Hash
+      first_item.must_include 'id'
+      first_item.must_include 'name'
+      first_item.must_include 'sources'
+    end
+
+    describe 'sources' do
+      let(:first_item) { client.projects[0] }
+      it 'must have at least one item with specific properties' do
+        first_item['sources'].must_be_instance_of Array
+        source = first_item['sources'][0]
+        source.must_include 'id'
+        source.must_include 'name'
+      end
     end
   end
 
   # TODO: add test for topics
   describe 'get topics' do
-    # let(:client) { Summics::Client.new('asdf', 'asdf') }
 
     before do
-      VCR.insert_cassette 'projects', :record => :new_episodes
+      VCR.insert_cassette 'topics', :record => :new_episodes
     end
 
     after do
       VCR.eject_cassette
+    end
+
+    it 'must have a projects method' do
+      client.must_respond_to :topics
     end
   end
 
