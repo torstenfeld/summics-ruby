c28fd7a03f1b2f01f1cb8fc7ee0a88c1ecbc2911
Torsten Feld <torsten@torsten-feld.de>
2015-06-19 15:36:00 +0200

    preparation for minitest extensions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require_relative '../lib/summics'
 gem 'minitest'
 require 'minitest/autorun'
+# require 'minitest/documentation'  # https://github.com/teoljungberg/minitest-documentation
+# require 'minispec-metadata'  # https://github.com/ordinaryzelig/minispec-metadata
+# require 'minitest-spec-context'  # https://github.com/ywen/minitest-spec-context
 require 'webmock/minitest'
 require 'vcr'
 
